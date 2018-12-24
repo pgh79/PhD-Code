8581e1b3f212d8a2e4031cbc6eeb118152467f7b
Demetri <dpananos@gmail.com>
2018-12-24 16:18:33 -0500

    proposal

--- 2018-09 Apixiban Bayesian Models/Run Models/Verify_Delay_Priors.R
+++ 2018-09 Apixiban Bayesian Models/Run Models/Verify_Delay_Priors.R
@@ -4,7 +4,7 @@ library(tidyverse)
 stan_program = '2018-09 Apixiban Bayesian Models/Delayed Models 1 Compartment/simulate_delays.stan'
 
 sims = stan(stan_program,
-            data = list(A=2.5, B = 2.5, ymin = 10, Z =1.5),
+            data = list(A=5.5, B = 1.5, ymin = 10, Z =1.5),
             init = 19920908,
             algorithm = 'Fixed_param',
             chains =1,
@@ -12,7 +12,7 @@ sims = stan(stan_program,
             )
 
 
-params = extract(sims)
+params = rstan::extract(sims)
 
 
 hist(params$delay, breaks = 20,probability = T)
